96531af17675d413af0c8014bc617bb4c751a2ff
devMeno <charlesamenlokonon@gmail.com>
2023-04-26 23:34:24 +0200

    Suite du projet

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var bou = document.getElementsByName("chiffre"),
+  operator = document.getElementsByName("operateur"),
   container = document.getElementById("resultBox"),
-  add = document.getElementById("addition");
-//console.log(container.innerText);
+  result = document.getElementById("resultat");
+
+console.log(operator);
+
+/*var tab = container.innerText.split("");
+console.log(tab);*/
 
 bou.forEach((element) => {
   element.addEventListener("click", function () {
@@ -12,3 +17,27 @@ bou.forEach((element) => {
     }
   });
 });
+
+operator.forEach((element) => {
+  element.addEventListener("click", function () {
+    if (container.innerText != 0) {
+      container.innerText = container.innerText + " " + this.innerText + " ";
+    }
+  });
+});
+
+function calculerResultat() {
+  let contenu = container.innerText;
+  let princaipalTab = contenu.split(" ");
+
+  princaipalTab.for (let index = 0, index < princaipalTab.length; index++) {
+    if(index%2 == 0){
+      parseFloat(princaipalTab[index]);
+      console.log(parseFloat(princaipalTab[index]))
+    }
+  }
+}
+
+calculerResultat();
+/*var contenu = container.innerText;
+console.log(contenu.split(" "));*/
